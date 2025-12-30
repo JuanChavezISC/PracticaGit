596a2a6b806c0d799e8a75812d646f44f39fabc9
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-12-30 12:32:16 -0600

    Se agrego clase Circulo y se agrego menu a clase main

--- src/Circulo/Circulo.java
+++ src/Circulo/Circulo.java
@@ -1,4 +1,19 @@
 package Circulo;
 
+//Calcular el area de un circulo
 public class Circulo {
+    private double r;
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public double calcularArea() {
+
+        return Math.PI * r * r;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
+import Circulo.Circulo;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+
+        do {
+            System.out.println("Bienvenido al menu para calcular areas");
+            System.out.println("Selecciona alguna de las siguientes opciones");
+            System.out.println("\n1. Circulo");
+            System.out.println("2. Cuadrado");
+            System.out.println("3. Rectangulo");
+            System.out.println("4. Triangulo");
+            System.out.println("5. Salir");
+
+            opcion = sc.nextInt();
+            switch (opcion){
+                case 1:
+                    Circulo c = new Circulo();
+                    System.out.println("Ingresa el radio: ");
+                    c.setR(sc.nextDouble());
+                    System.out.println("Area del circulo: " + c.calcularArea());
+                    break;
+
+                case 5:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida");
+            }
+
+        }while (opcion != 5);
+
     }
 }
