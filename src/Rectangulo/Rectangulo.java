cd3a7e1183f27f91fa11bedcee2920bda2c2bcc4
Dano <cd.bolanunez@gmail.com>
2025-12-30 11:53:59 -0600

    Se agrega metodos a las clases triangulo y rectangulo

--- src/Rectangulo/Rectangulo.java
+++ src/Rectangulo/Rectangulo.java
@@ -1,5 +1,30 @@
 package Rectangulo;
 
 public class Rectangulo {
-
+	//metod para calcular el area de un rectangulo
+	// area = base por altura
+	
+	double base;
+	double altura;
+	public double getBase() {
+		return base;
+	}
+	public void setBase(double base) {
+		this.base = base;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public Rectangulo(double base, double altura) {
+		super();
+		this.base = base;
+		this.altura = altura;
+	}
+	
+	public double calcularArea() {
+		return (base*altura);
+	}
 }
--- src/Triangulo/Triangulo.java
+++ src/Triangulo/Triangulo.java
@@ -2,4 +2,37 @@ package Triangulo;
 
 public class Triangulo {
 
+	
+	// metodo para obtener el area de un triangulo 
+	//recordar que el area del triangulo es base*altura/2
+	
+	double base;
+	double altura;
+	
+	
+	public Triangulo(double base, double altura) {
+		super();
+		this.base = base;
+		this.altura = altura;
+	}
+
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public double calcularArea() {
+		return (base*altura)/2;
+	}
 }
